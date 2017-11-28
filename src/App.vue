7cb54cfb962b04b25cc056ab23aeae08a5a10b13
Valentin Krasontovitsch <valkra@ihelse.net>
2017-11-28 15:50:53 +0100

    Feat: Add layout header, fix margins

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <header class="mdc-toolbar">
+      <div class="mdc-toolbar__row">
+        <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
+          <span class="mdc-toolbar__title">MGM IT Projects - Documentation</span>
+        </section>
+      </div>
+    </header>
     <router-view/>
   </div>
 </template>
@@ -11,12 +18,14 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
