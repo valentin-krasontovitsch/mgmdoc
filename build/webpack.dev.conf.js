97033e78811e6d7f3f0674e11f21c5f1eff91ea3
Valentin Krasontovitsch <valkra@ihelse.net>
2017-11-28 15:42:26 +0100

    Fix: Enable accessing dev server from other machines

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -22,6 +22,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     hot: true,
     host: process.env.HOST || config.dev.host,
     port: process.env.PORT || config.dev.port,
+    disableHostCheck: true,
     open: config.dev.autoOpenBrowser,
     overlay: config.dev.errorOverlay ? {
       warnings: false,
