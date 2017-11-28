7462d44c3c6dabfe0a372dfaec3e521944894439
Valentin Krasontovitsch <valkra@ihelse.net>
2017-11-28 15:53:07 +0100

    Fix: Make compatible with IE11

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,14 @@
-// import Promise from 'es6-promise'
-// Promise.polyfill
-// require('string.prototype.startswith');
-// require('../my_material.scss')
-// require('../common.scss')
+import Promise from 'es6-promise'
 import Vue from 'vue'
 import Axios from 'axios'
 import Router from 'vue-router'
 import FilteredList from '@/components/filtered_list.vue'
+require('string.prototype.startswith')
+require('../common.scss')
+require('../my_material.scss')
 require('material-components-web/dist/material-components-web.css')
 Vue.prototype.$http = Axios
-console.log('I am application.js, entrypoint for all?')
-
+Promise.polyfill()
 Vue.use(Router)
 
 export default new Router({
